f0eba9dbb512024c866aecf984ef68d43055b678
kazuho <kazuho@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-14 11:09:35 +0000

    add missing
    git-svn-id: http://svn.coderepos.org/share/lang/perl/NanoA/trunk@23700 d0d07461-0603-4401-acd4-de1884942a52

---
+++ nanoa.cgi
@@ -0,0 +1,15 @@
+#! /usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib qw(lib);
+
+use NanoA;
+use NanoA::Config;
+use NanoA::DebugScreen;
+use NanoA::Dispatch;
+use NanoA::TemplateLoader;
+
+require 'nanoa.pl';
+1;
---
+++ nanoa.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+use utf8;
+
+if ($ENV{MOD_PERL}) {
+    my $base_dir = $ENV{SCRIPT_FILENAME};
+    $base_dir =~ s|/[^/]*$||;
+    chdir $base_dir;
+}
+unshift @INC, 'extlib';
+
+do {
+    local $@;
+    local $SIG{__DIE__} = sub {
+        NanoA::DebugScreen::build(@_);
+    };
+    eval {
+        NanoA::Dispatch->dispatch();
+    };
+    if ($@) {
+        if (ref $@ eq 'HASH') {
+            NanoA::DebugScreen::output($@);
+        } else {
+            die $@;
+        }
+    }
+};
+
+1;
