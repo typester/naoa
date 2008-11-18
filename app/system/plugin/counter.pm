696c6067773629147f165de3e6669ac843d7a7c0
kazuho <kazuho@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-18 06:31:03 +0000

    always use system prefs
    git-svn-id: http://svn.coderepos.org/share/lang/perl/NanoA/trunk@24091 d0d07461-0603-4401-acd4-de1884942a52

--- app/system/plugin/counter.pm
+++ app/system/plugin/counter.pm
@@ -7,11 +7,14 @@ sub run_as {
     my $counter_name = $c && $c->{name} || ref $app;
     $counter_name =~ s/::/./g;
     
+    # config オブジェクトを取得 (今回は system の config に保存)
+    my $config = $app->config->system_config;
+    
     # カウンタの値を設定データベースからロード
-    my $cnt = $app->config->prefs("counter.$counter_name") || 1;
+    my $cnt = $config->prefs("counter.$counter_name") || 1;
     
     # +1 した値を保存
-    $app->config->prefs("counter.$counter_name", $cnt + 1);
+    $config->prefs("counter.$counter_name", $cnt + 1);
     
     # カウンタの値を返す
     $cnt;
