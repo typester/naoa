02e6fbd3d0d7686c99cc683df1e3f353287a6735
kazuho <kazuho@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-30 08:25:38 +0000

    customizable removal of "use XXX"
    git-svn-id: http://svn.coderepos.org/share/lang/perl/NanoA/trunk@25414 d0d07461-0603-4401-acd4-de1884942a52

--- tools/concat.pl
+++ tools/concat.pl
@@ -1,12 +1,31 @@
+#! /usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+
+my $no_use = '';
+GetOptions(
+    'no-use=s' => \$no_use,
+) or exit 1;
+$no_use = { map { +($_ => 1) } split /\s*,\s*/, $no_use };
+
+
 print "#!$^X\n";
-foreach my $file (@ARGV) {
+
+my @files = @ARGV;
+$no_use->{$_} = 1
+    for map { my $n = $_; $n =~ s/\.pm$//; $n =~ s|/|::|g; $n } @files;
+
+foreach my $file (@files) {
     open my $fh, '<', $file
         or die "open($file):$!";
     while (<$fh>) {
         last if /^(__END__|"ENDOFMODULE";)$/;
-        next if /^\s*$/;
-        next if /^use\s+(strict|warnings);$/;
-        print
+        if (/^use\s+([^ ]+);$/) {
+            next if $no_use->{$1};
+        }
+        print;
     }
     close $fh;
 }
